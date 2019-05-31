a6b9ae207a96d5d13693ba05b39dd60c9883b148
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-31 10:43:44 +0200

    Initialize view

---
+++ src/main/java/View.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+public class View {
+
+    private JFrame frame;
+    private JPanel mainPanel;
+
+    public View(String title) {
+        frame = new JFrame(title);
+        createMenu();
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private void createMenu() {
+        JMenuBar menuBar = new JMenuBar();
+
+//        File
+        JMenu file = new JMenu("File");
+        menuBar.add(file);
+
+        JMenuItem file_item_1 = new JMenuItem("Select source");
+
+        file.add(file_item_1);
+        frame.setJMenuBar(menuBar);
+    }
+}
